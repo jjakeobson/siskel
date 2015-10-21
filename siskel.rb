735289b0842a111e4d6b99ea8b4a26da17cca56b
jjakeobson <jjakeobson@gmail.com>
2015-10-21 14:42:01 -0400

    6/10 working

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,21 @@
 require 'httparty'
 
 class Siskel
-  attr_reader :title
+  attr_reader :title, :rating, :year, :plot
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
+  def initialize(title, options={})  #same as year={}
+    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{options[:year]}")
     @title = @movie['Title']
+    @rating = @movie['Rated']
+    @year = @movie['Year']
+    @plot = @movie['Plot']
+  end
+
+  def title
+    if @title == nil
+      @title = "Movie not found!"
+    else
+      @title
+    end
   end
 end
